2ac29de188f6e5acf9732b034911954b17d41ce6
ABHI <ABHI@HP-CS1000TX>
2024-03-19 01:30:35 +0530

    Created demoClass For Branching

---
+++ demoApps/src/test/java/demotest/DemoTest.java
@@ -0,0 +1,16 @@
+package demotest;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+	@Test
+	public void demoTest()
+	{
+		System.out.println("Hello World");
+	
+		System.out.println("Hello GitHUb branching");
+
+	}
+
+}
